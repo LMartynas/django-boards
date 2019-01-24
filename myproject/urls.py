e8e85190c5eee971d8a3e5b14bceae0ff1c2154d
Martynas Lukosevicius <c00219802@itcarlow.ie>
2019-01-24 17:43:09 +0000

    Committing after solving what kept me stuck for two days

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from boards import views
-from django.urls import path
+from django.urls import path , re_path
+
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
 ]
